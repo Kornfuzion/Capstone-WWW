fed1bfb335cb413693ed5b68e4982210c08839cf
Tom Zhang <tomzhang94@tgmail.com>
2017-03-10 04:27:47 -0500

    updated model route to take in an event id

--- src/routes.php
+++ src/routes.php
@@ -8,8 +8,16 @@ $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/model', function ($request, $response, $args) {
+$app->get('/model/{eventId}', function ($request, $response, $args) {
+	$eventId = $request->getAttribute('eventId');
+	$modelHost = "http://com.scope.model.s3.amazonaws.com/";
+	$modelPath = $modelHost . $eventId . "/"  . "1.ply"; //TODO: multiple frames
+    //var_dump($args);
+    //var_dump($response);
+    //$response->withAttribute('modelPath', $modelPath);
     // Render model viewer
+    //return $response->withStatus(200);
+    $args['modelPath'] = $modelPath;
     return $this->renderer->render($response, 'model-viewer.phtml', $args);
 });
 
