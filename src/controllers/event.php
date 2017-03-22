1c9e548bd54b147b3cc52d8cb9c60c262e5d74dc
Tom Zhang <tomzhang94@tgmail.com>
2017-03-22 01:20:34 -0400

    cleaned up broadcast code. Kept previous route in routes for backwards compability for now

--- src/controllers/event.php
+++ src/controllers/event.php
@@ -8,7 +8,8 @@ class EventController {
         $this->container = $app->getContainer(); 
         $app->get('/events', array($this, 'getNearestEvents'));
         $app->get('/events/archive', array($this, 'getArchivedEvents'));
-        $app->get('/event/{id}', array($this, 'getEventByID')); 
+        $app->get('/event/{id}', array($this, 'getEventByID'));
+        $app->get('/event/capture/{id}', array($this, 'initiateImageCapture')); 
         $app->post('/event/{id}', array($this, 'createEvent'));
         $app->put('/event/{id}', array($this, 'editEvent'));
         $app->get('/event/join/{id}', array($this, 'joinEvent'));
@@ -46,6 +47,36 @@ class EventController {
         );
 
         return $event;
+    }
+
+    function sendBroadcast($title, $body, $topic) {
+        $url = "https://fcm.googleapis.com/fcm/send";
+
+        $data = array(
+            'data' => array(
+                'title' => $title,
+                'body' => $body      
+            ),
+            'to' => "/topics/$topic" 
+        );
+
+        $content = json_encode($data, JSON_UNESCAPED_SLASHES);
+        $requestHeaders = array(
+            'Content-type: application/json',
+            'Authorization: key=AAAAkH7zsZQ:APA91bERB53BhlshsRLZ1p82VSih4d_GDs9eE9jdCaRiCFXhNls1_EZAmVBhNMyYF_iDANy6m9ReJ_PIfdoQlRsy5lM1wGI5V0EhuajBrmZeXDjPXlAwt2Olns3VnyTBny5UWjk3Uhdi',
+            sprintf('Content-Length: %d', strlen($content))
+        );
+
+        // use key 'http' even if you send the request to https://...
+        $options = array(
+            'http' => array(
+                'header'  => implode("\r\n", $requestHeaders), 
+                'method'  => 'POST',
+                'content' => $content
+            )
+        );
+        $context  = stream_context_create($options);
+        return file_get_contents($url, false, $context);
     }
 
     function getNearestEvents($request, $response, $args) {
@@ -196,6 +227,12 @@ class EventController {
 
         $event = self::parseItem($result['Attributes']); 
         return $response->withJson($event);
+    }
+
+    function initiateImageCapture($request, $response, $args) {
+        $eventId = $request->getAttribute('id');
+        $result = sendBroadcast("Capture", $eventId, $eventId);
+        return $response->withJson($result);
     }
 
     function contributeToEvent($request, $response, $args) {
--- src/routes.php
+++ src/routes.php
@@ -3,19 +3,46 @@
 
 require __DIR__ . '/controllers/event.php';
 require __DIR__ . '/controllers/model.php';
-require __DIR__ . '/php/sendBroadcast.php';
 
 $app->get('/', function ($request, $response, $args) {
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
+function sendBroadcast($title, $body, $topic) {
+    $url = "https://fcm.googleapis.com/fcm/send";
+
+    $data = array(
+        'data' => array(
+            'title' => $title,
+            'body' => $body      
+        ),
+        'to' => "/topics/$topic" 
+    );
+
+    $content = json_encode($data, JSON_UNESCAPED_SLASHES);
+    $requestHeaders = array(
+        'Content-type: application/json',
+        'Authorization: key=AAAAkH7zsZQ:APA91bERB53BhlshsRLZ1p82VSih4d_GDs9eE9jdCaRiCFXhNls1_EZAmVBhNMyYF_iDANy6m9ReJ_PIfdoQlRsy5lM1wGI5V0EhuajBrmZeXDjPXlAwt2Olns3VnyTBny5UWjk3Uhdi',
+        sprintf('Content-Length: %d', strlen($content))
+    );
+
+    // use key 'http' even if you send the request to https://...
+    $options = array(
+        'http' => array(
+            'header'  => implode("\r\n", $requestHeaders), 
+            'method'  => 'POST',
+            'content' => $content
+        )
+    );
+    $context  = stream_context_create($options);
+    return file_get_contents($url, false, $context);
+}
+
 $app->get('/broadcast', function ($request, $response, $args) {
-    // Render index view
     $topic = $request->getQueryParam('topic');
-    sendBroadcast(0, 0, $topic);	
-    return $response->withStatus(200);
-    //return $this->renderer->render($response, 'sendBroadcast.php', $args);
+    $result = sendBroadcast(0,0, $topic);
+    return $response->withJson($result);
 });
 
 new EventController($app);
